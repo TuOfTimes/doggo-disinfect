4adb300d819d6ce96434e33e324879916b0b8314
Edward <TuOfTimes@github.com>
2018-11-10 14:59:31 -0500

    change all images to dogs

--- content.js
+++ content.js
@@ -4,15 +4,12 @@
     <img src="https://source.unsplash.com/1600x900/?corgi,samoyed,dog" alt="">
     We can allow users to specif 
 */
-window.onload = function() {
-    document.body.style.position = 'static';
-    document.body.style.overflow = 'visible';
-    if (chrome.extension.inIncognitoContext) {
-        var modal = document.getElementsByClassName('gip__modal');
-        modal[0].parentNode.removeChild(modal[0]);
+function nodeInsertedCallback(event) {
+    console.log(event);
+    var imgs = document.getElementsByTagName("img");
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].src = "https://source.unsplash.com/1600x900/?corgi,samoyed,dog";
     }
-    else {
-        var paywall = document.getElementsByClassName('meter-paywall');
-        paywall[0].parentNode.removeChild(paywall[0]);
-    }
-}
+  };
+
+document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
