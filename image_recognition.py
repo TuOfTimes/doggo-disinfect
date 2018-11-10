fbe0fa110d1183d1845efdfa32ebfd2a4008fdc4
Brenden Kadota <brenden.kadota@gmail.com>
2018-11-10 14:47:33 -0500

    updated image_recognition to take arguments

--- image_recognition.py
+++ image_recognition.py
@@ -1,13 +1,23 @@
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
+import sys
 
 API_KEY = "ab2d666b14614d2db6c1041798af08ee"
 
+def main():
 
-app = ClarifaiApp( api_key = API_KEY )
+    app = ClarifaiApp( api_key = API_KEY )
 
-model = app.models.get('nsfw-v1.0')
+    model = app.models.get('nsfw-v1.0')
 
-image = ClImage( url='https://samples.clarifai.com/nsfw.jpg' )
+    image = sys.argv[1]
+    print image
+    response = model.predict_by_url(url=image)
 
-print model.predict([image])
+    sfw =  response["outputs"][0]["data"]["concepts"][0]
+    nsfw = response["outputs"][0]["data"]["concepts"][1]
+    print "sfw value " + str(sfw["value"])
+    print "nsfw value " + str(nsfw["value"])
+
+if __name__ == "__main__":
+    main()
